78afff8ced07b5419a2b27fb16464d2b78e6b6a4
juridon <juri.minamiyama@gmail.com>
2025-07-31 10:26:48 +0900

    セミコロン削除

--- src/index.tsx
+++ src/index.tsx
@@ -8,18 +8,18 @@ interface KintoneEvent {
   record: kintone.types.SavedFields
 }
 
-const roots = new Map<HTMLElement, ReturnType<typeof createRoot>>();
+const roots = new Map<HTMLElement, ReturnType<typeof createRoot>>()
 
 function renderReactComponent(container: HTMLElement, element: React.ReactElement) {
   if (!roots.has(container)) {
-    roots.set(container, createRoot(container));
+    roots.set(container, createRoot(container))
   }
-  roots.get(container)!.render(element);
+  roots.get(container)!.render(element)
 }
 
 kintone.events.on('app.record.index.show', (event: KintoneEvent) => {
-  const headerMenuSpaceElement = kintone.app.getHeaderMenuSpaceElement();
-  if(!headerMenuSpaceElement) return event;
+  const headerMenuSpaceElement = kintone.app.getHeaderMenuSpaceElement()
+  if(!headerMenuSpaceElement) return event
   renderReactComponent(headerMenuSpaceElement, <AppSwitcher />);
   return event
 })
@@ -27,13 +27,14 @@ kintone.events.on('app.record.index.show', (event: KintoneEvent) => {
 kintone.events.on('app.record.detail.show', (event: KintoneEvent) => {
   let query = `parent = ${event.record.$id.value} or $id= ${event.record.$id.value}`
   event.record.parent.value && (query += ` or $id = ${event.record.parent.value}`)
-  const headerMenuSpaceElement = kintone.app.getHeaderMenuSpaceElement();
-  if(!headerMenuSpaceElement) return event;
-  renderReactComponent(headerMenuSpaceElement, <GanttCharts query={query} />);
+  
+  const headerMenuSpaceElement = kintone.app.getHeaderMenuSpaceElement()
+  if(!headerMenuSpaceElement) return event
+  renderReactComponent(headerMenuSpaceElement, <GanttCharts query={query} />)
 
-  const addSubSpaceElement = kintone.app.record.getSpaceElement('addSub');
-  if(!addSubSpaceElement) return event;
-  renderReactComponent(addSubSpaceElement, <AddSub id={event.record.$id.value} />);
+  const addSubSpaceElement = kintone.app.record.getSpaceElement('addSub')
+  if(!addSubSpaceElement) return event
+  renderReactComponent(addSubSpaceElement, <AddSub id={event.record.$id.value} />)
 
   return event
 })
